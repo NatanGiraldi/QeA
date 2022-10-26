c17978fb3c776c698467abd20904d9d01d3f45fd
NatanGiraldi <natangiraldi@gmail.com>
2022-10-26 17:48:43 -0300

    zsd

--- operations/insertion.php
+++ operations/insertion.php
@@ -1,8 +1,20 @@
 <?php 
-session_start();
 
+session_start();
     $materia = $_SESSION['materia'];
 
-    print "Materia: $materia</br>";
-    echo "insertion of question";
 ?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head><meta charset="UTF-8"></head>
+<body>
+    <div> 
+
+        <h2>Adicionar pergunta</h2>
+        <h4>Disciplina: <?php print "$materia"; ?> </h4><br/>
+        <label>Titulo:</label>
+
+    </div>
+</body>
+</html>
